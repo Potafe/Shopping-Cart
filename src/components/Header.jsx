291cd92fd09679b9e5e5ac45825afeb50778fc6f
Potafe <bt21me011@nitmz.ac.in>
2024-03-29 23:14:04 +0530

    refactor: use prettier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header className="border-b border-gray-200">
-            <nav>
-                <ul>
-                    <NavLink to = "/">Home</NavLink>
-                    <NavLink to = "/store">Store</NavLink>
-                    <NavLink to = "/about">About</NavLink>
-                    <NavLink to = "/contact">Contact Us</NavLink>
-                </ul>
-            </nav>
-        </header>
-    )   
-}
+  return (
+    <header className="border-b border-gray-200">
+      <nav>
+        <ul>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/store">Store</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact Us</NavLink>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
