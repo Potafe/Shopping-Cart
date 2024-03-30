36a7d098d1730554797429f2140a19eeb43dabca
Potafe <bt21me011@nitmz.ac.in>
2024-03-30 21:46:56 +0530

    feat: add background image

---
+++ src/components/Scroller.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useScrollDirection = () => {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    let oldScrollY = window.scrollY;
+    function getScrollDirection() {
+      const currScrollY = window.scrollY;
+      const direction = currScrollY > oldScrollY ? "down" : "up";
+      if (direction !== scrollDirection && Math.abs(oldScrollY - currScrollY) > 4) {
+        setScrollDirection(direction);
+      }
+
+      oldScrollY = currScrollY;
+    }
+
+    window.addEventListener("scroll", getScrollDirection);
+
+    return () => {
+      window.removeEventListener("scroll", getScrollDirection);
+    };
+  });
+
+  return scrollDirection;
+}
+
+export default useScrollDirection;
