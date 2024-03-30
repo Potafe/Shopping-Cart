2c8491ff3bd19f1160b1054dd5f061d6eff74481
Potafe <bt21me011@nitmz.ac.in>
2024-03-30 14:48:52 +0530

    feat: add cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Store from "./components/Store/MainStore";
+import Cart from "./components/Cart";
+import { useState } from "react";
 
 function App() {
+  const [isCartActive, setIsCartActive] = useState(false);
+
+  const toggleCart = () => setIsCartActive(!isCartActive);
+  
   return (
     <>
-      <Header />
+      {isCartActive && <Cart onClose={toggleCart} />}
+      <Header onOpenCart={toggleCart} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
