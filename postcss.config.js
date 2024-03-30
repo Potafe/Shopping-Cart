12cac7d4628c232d2bdd1fbec7bfa15d769bfe6e
Potafe <bt21me011@nitmz.ac.in>
2024-03-30 12:55:53 +0530

    fix: tailwing styling error

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export const plugins = {
-  tailwindcss: {},
-  autoprefixer: {},
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
