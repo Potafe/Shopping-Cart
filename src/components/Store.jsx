2ec218366cf957894737fe95379ba883f1c98bc4
Potafe <bt21me011@nitmz.ac.in>
2024-03-30 11:58:55 +0530

    fix: product category section now displaying

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -9,6 +9,8 @@ const Store = () => {
   const { category } = useParams();
   const [products, setProducts] = useState(null);
   const [categories, setCategories] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
 
   useEffect(() => {
     (async () => {
@@ -20,18 +22,27 @@ const Store = () => {
       );
 
       if (categories === null) {
-        const categoriesData = await fetch(
+        const categoriesData = await fetchData(
           "https://fakestoreapi.com/products/categories",
         );
         setCategories(categoriesData);
       }
       setProducts(products);
+      setIsLoading(false);
     })();
   }, [category]);
 
   if (products === null) {
     return <Loader />;
-  } else {
+  } 
+    
+  const changeProductCategory = () => {
+    setIsLoading(true);
+  };
+
+  const greyedStyles = isLoading && "opacity-30";
+
+
     return (
       products && (
         <>
@@ -39,11 +50,33 @@ const Store = () => {
             <div className="w-52 shrink-0 leading-5">
               {/* Sidebar */}
               <h2 className="text-xl font-black pb-5">Categories</h2>
+              {category && (
+                <Link
+                  to="/store"
+                  className="text-sm font-medium flex items-center gap-x-2 px-3 py-2 border border-red-500 text-red-500 bg-red-50 mb-2"
+                  onClick={changeProductCategory}
+                >
+                  <svg
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                  Clear Category
+                </Link>
+              )}
               <ul>
                 {categories.map((cat, index) => {
                   return (
-                    <li className="pb-1" key={index}>
-                      <Link to={`/shop/category/${cat}`}>{cat}</Link>
+                    <li className="mb-2 capitalize" key={index}>
+                      <Link to={`/store/category/${cat}`} onClick={changeProductCategory} >{cat}</Link>
                     </li>
                   );
                 })}
@@ -52,10 +85,10 @@ const Store = () => {
 
             <div>
               <h1 className="text-5xl mb-8 tracking-tight">
-                World-class{" "}
-                <span className="font-extrabold">composable businesses.</span>
+                Unlock <span className="font-extrabold">Innovation</span>, Shop
+                Your Future Today!
               </h1>
-              <div className="grid grid-cols-3 gap-5">
+              <div className={`grid grid-cols-3 gap-5 ${greyedStyles}`}>
                 {products.map((product) => {
                   return (
                     <div className="flex flex-col p-5 gap-y-1" key={product.id}>
@@ -71,6 +104,6 @@ const Store = () => {
         </>
       )
     );
-  }
-};
+  };
+
 export default Store;
